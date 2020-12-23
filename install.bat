b074d4aa1770fe0d8022bfe121ce0fba9dbcb641
Ben Johnson <bdjohnson529@gmail.com>
2020-12-22 17:29:13 -0800

    change install.bat file

--- install.bat
+++ install.bat
@@ -1,8 +1,8 @@
 :: virtual environment
-:: for some reason installation does not work in virtual environment
-:: call conda remove -y -n plotlydash --all
-:: call conda create -y -n plotlydash python
-:: call activate plotlydash
+:: for some reason installation does not work on some machines
+call conda remove -y -n plotlydash --all
+call conda create -y -n plotlydash python
+call activate plotlydash
 
 :: install requirements
 call pip install -r requirements.txt
